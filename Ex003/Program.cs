b96aa2da18595d87d0a2aade5f4630c2a2bfcf6d
Alex <geni46@mail.ru>
2022-12-06 02:50:52 +0500

    Ex003 Random1

---
+++ Ex003/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = new Random().Next(0, 1000); 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(0, 1000);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
---
+++ Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "григорий")
+{
+    Console.WriteLine("ОГО Грирорий! Какими судьбами?! Всегда тебе рады, старый пес!)");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
